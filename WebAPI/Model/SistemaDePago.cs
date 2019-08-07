6170554455b1a5bad40f98e26041cd85a2917e6f
Gerald Paulino Rodriguez <gerald_pr15@hotmail.com>
2019-08-06 21:08:43 -0400

    Creacion de las estructuras base

---
+++ WebAPI/Model/SistemaDePago.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class SistemaDePago
+    {
+        public int Pago_ID { get; set; }
+        public string Nombre_Tarjeta { get; set; }
+        public string Numero_Tajerta { get; set; }
+        public string Dia_Expiracion { get; set; }
+        public string CVV { get; set; }
+    }
+}
---
+++ WebAPI/Persistence/SistemaDePagoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class SistemaDePagoContext : DbContext
+    {
+        public SistemaDePagoContext(DbContextOptions<SistemaDePagoContext> options) 
+            : base(options)
+        { }
+
+        public DbSet<SistemaDePago> SistemaDePago { get; set; }
+    }
+}
